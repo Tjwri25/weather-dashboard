a9dc023241db9f193f8c96eb4c1bca30df068822
Taylor Wright <tjwri25@gmail.com>
2021-05-13 15:44:39 -0600

    fetch working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,72 @@
-
-$(document).ready(function () {
-
+var apiURL = "0314bd76bd07321db823b61641900d3d"
+var citySearch = document.querySelector("#city-search");
 var searchButton = $("#search-button");
 var cityList =$("#list-cities");
-var searchInput = $("#city-search").val();
-var city = [];
+let lat;
+let long;
+let day1 = document.getElementById("dayOne");
+let day2 = document.getElementById("dayTwo"); 
+let day3 = document.getElementById("dayThree"); 
+let day4 = document.getElementById("dayFour");
+let day5 = document.getElementById("dayFive");
+var weatherIcons = {
+  sunny: "☀️",
+  cloudy: "☁",
+  partlySunny: "⛅",
+  rainy: "🌧",
+  snowy: "❄️",
+  stormy: "⛈",
+  windy: "🌬",
+  foggy: "🌫",
+}
 
-var apiKey = "0314bd76bd07321db823b61641900d3d"
 
+  
 
 searchButton.on("click", function () {
-//   console.log("clicked")
+  console.log("clicked")
     
     var searchInput = $("#city-search").val();
     localStorage.setItem("city", JSON.stringify(searchInput));
 
 });
 
-function makeList() {
-    var listItem = $("<li>").text(searchInput);
-    $(cityList).append(listItem);
-  };
-//  fetch ('api.openweathermap.org/data/2.5/weather?q= '+inputValue.value+'&appid=0314bd76bd07321db823b61641900d3d')
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-})
+// function makeList() {
+//     var searchInput = $("#city-search").val();
+//     var listItem = $("<li>").text(searchInput);
+//     $(cityList).append(listItem);
+//     makeList();
+//   };
+// //  fetch ('api.openweathermap.org/data/2.5/weather?q= '+inputValue.value+'&appid=0314bd76bd07321db823b61641900d3d')
+// //     .then(response => response.json())
+// //     .then(data => console.log(data))
+
+// $(searchButton).on("click", getApi)
+
+
+
+function getApi() {
+  var city = citySearch.value.trim();
+  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiURL}`;
+  fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  }) 
+  .then(function (data) {
+    console.log(data)
+
+
+  })
+
+}
+$(searchButton).on("click", getApi)
+
+
+
+
+
+
+
+
+
+
